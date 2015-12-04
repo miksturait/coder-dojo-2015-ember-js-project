762df9142ee8f112695f6844748d4f57a6484ba3
Michal Czyz <michalczyz@gmail.com>
2015-12-04 14:47:22 +0100

    Route :: load coworkers and kudos at once
    (watch in developer tools how objects are loaded from net)

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -3,8 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      coworkers: this.store.findAll('coworker'),
-      kudos: this.store.findAll('kudo')
+      coworkers: this.store.queryRecord('coworker', {include: 'kudos'})
     });
   }
 });
