2cbcbc505ff18d89557ccb1a651974c5b92ff26f
Michal Czyz <michalczyz@gmail.com>
2015-12-09 13:54:35 +0100

    ROUTING :: preloading nested route

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -5,5 +5,10 @@ export default Ember.Route.extend({
     return Ember.RSVP.hash({
       coworkers: this.store.queryRecord('coworker', {include: 'kudos'})
     });
+  },
+  afterModel(model, transition) {
+    if (model.coworkers) {
+      this.transitionTo('dashboard.index.coworker', model.coworkers.get('firstObject'));
+    }
   }
 });
