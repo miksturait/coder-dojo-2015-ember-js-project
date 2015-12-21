f9e70481224848d5f2b56a200868de694b8d79e0
Michal Czyz <michalczyz@gmail.com>
2015-12-21 13:20:17 +0100

    ember g model coworker email:string firstName:string lastName:string avatarUrl:string kudos:hasMany

---
+++ app/models/coworker.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  email: DS.attr('string'),
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  avatarUrl: DS.attr('string'),
+  kudos: DS.hasMany('kudo')
+});
