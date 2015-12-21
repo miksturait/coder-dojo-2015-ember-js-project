717774b4f0b55e13b2a4bc1ddd6a7c6e50e78108
Michal Czyz <michalczyz@gmail.com>
2015-12-21 13:52:41 +0100

    ember g component --pod coworkers/navigation-element

--- app/models/coworker.js
+++ app/models/coworker.js
@@ -5,5 +5,13 @@ export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   avatarUrl: DS.attr('string'),
-  kudos: DS.hasMany('kudo')
+  kudos: DS.hasMany('kudo'),
+
+  fullName: Ember.computed('fistName', 'lastName', function() {
+    return `${this.get('firstName')} ${this.get('lastName')}`;
+  }),
+
+  kudosCollected: Ember.computed('kudos.@each.value', function() {
+    return this.get('kudos').reduce(((acc, kudo) => acc + kudo.get('value')), 0);
+  })
 });
