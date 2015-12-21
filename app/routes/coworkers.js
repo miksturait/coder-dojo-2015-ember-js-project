32f0da3e295ca74a58d342034c207b8bf4ca2c58
Michal Czyz <michalczyz@gmail.com>
2015-12-21 14:26:46 +0100

    let's load all data at once

--- app/routes/coworkers.js
+++ app/routes/coworkers.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('coworker');
+    return this.store.query('coworker', {include: 'kudos'} );
   }
 });
