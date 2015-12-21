c63a87d302ac02063f05992583ecb9d77cc3a9e7
Michał Czyż <michalczyz@MacBook-Pro-Micha.local>
2015-12-21 18:50:27 +0100

    adding csv transformer

--- app/models/uczen.js
+++ app/models/uczen.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   databaseId: DS.attr('number'),
   name: DS.attr('string'),
-  oceny: DS.attr('string'),
+  oceny: DS.attr('csv-array'),
   obecny: DS.attr('boolean')
 });
