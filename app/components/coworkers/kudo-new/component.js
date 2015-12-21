907ec3e68f6ab10d999366200f990b700dba81e3
Michal Czyz <michalczyz@gmail.com>
2015-12-21 15:37:58 +0100

    trying to handle errors

--- app/components/coworkers/kudo-new/component.js
+++ app/components/coworkers/kudo-new/component.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
   kudo: null,
+  errors: null,
 
   init() {
     this._setKudo();
@@ -13,13 +14,18 @@ export default Ember.Component.extend({
     create() {
       let kudo = this.get('kudo');
       kudo.set('coworker', this.get('coworker'));
-      kudo.save();
-      this._setKudo();
+      kudo.save().then(function (response) {
+        this._setKudo();
+        this.set('errors', null);
+      }, function (response) {
+        kudo.set('coworker', null);
+        // this.set('errors', response.errors.mapBy('detail').join(' '));
+      });
     }
   },
 
   _setKudo() {
     this.set('kudo',
-             this.get('store').createRecord('kudo', {value: 1, comment: ':-)'}));
+      this.get('store').createRecord('kudo', {value: 1, comment: ':-)'}));
   }
 });
